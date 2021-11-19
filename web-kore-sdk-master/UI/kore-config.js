1830bd0433bc855cef5cdd673215ee3672b03009
unknown <praveen.tissera@gmail.com>
2021-11-19 11:21:31 +0530

    initial commit with source code

---
+++ web-kore-sdk-master/UI/kore-config.js
@@ -0,0 +1,77 @@
+(function(KoreSDK){
+
+    var KoreSDK=KoreSDK||{};
+
+    var botOptions = {};
+    botOptions.logLevel = 'debug';
+    botOptions.koreAPIUrl = "https://bots.kore.ai/api/";
+    botOptions.koreSpeechAPIUrl = "";//deprecated
+    //botOptions.bearer = "bearer xyz-------------------";
+    //botOptions.ttsSocketUrl = '';//deprecated
+    botOptions.koreAnonymousFn = koreAnonymousFn;
+    botOptions.recorderWorkerPath = '../libs/recorderWorker.js';
+
+    botOptions.JWTUrl = "http://localhost:3000/api/users/sts";
+    botOptions.userIdentity = 'praveen.tissera@gmail.com';// Provide users email id here
+    botOptions.botInfo = { name: "banking bot", "_id": "st-fc4f3da0-c206-5b55-beb6-62a03ffd399c" }; // bot name is case sensitive
+
+    /* 
+    Important Note: These keys are provided here for quick demos to generate JWT token at client side but not for Production environment.
+    Refer below document for JWT token generation at server side. Client Id and Client secret should maintained at server end.
+    https://developer.kore.ai/docs/bots/sdks/user-authorization-and-assertion/
+    **/
+    botOptions.clientId = "cs-e8f6a99d-6da9-5a60-9546-14e4cf2004db";
+    botOptions.clientSecret = "pI8Q0A/y769iQcufYrjVmoMDhAgmaXMJxdLuQDtR7T8=";
+
+// for webhook based communication use following option 
+// botOptions.webhookConfig={
+//     enable:true,
+//     webhookURL:'PLEASE_PROVIDE_WEBHOOK_URL',
+//     apiVersion:2 //webhookURL will be converted to v2 by default. To use v1(not recommended) webhookURL change it to 1
+// }
+   
+// To modify the web socket url use the following option
+// botOptions.reWriteSocketURL = {
+//     protocol: 'PROTOCOL_TO_BE_REWRITTEN',
+//     hostname: 'HOSTNAME_TO_BE_REWRITTEN',
+//     port: 'PORT_TO_BE_REWRITTEN'
+// };
+    
+    var chatConfig={
+        botOptions:botOptions,
+        allowIframe: false, 			// set true, opens authentication links in popup window, default value is "false"
+        isSendButton: false, 			// set true, to show send button below the compose bar
+        isTTSEnabled: false,			// set true, to hide speaker icon
+        ttsInterface:'webapi',        // webapi or awspolly , where default is webapi
+        isSpeechEnabled: true,			// set true, to hide mic icon
+        allowGoogleSpeech: true,		// set true, to use Google speech engine instead KORE.AI engine.This feature requires valid Google speech API key. (Place it in 'web-kore-sdk/libs/speech/key.js')
+        allowLocation: false,			// set false, to deny sending location to server
+        loadHistory: false,				// set true to load recent chat history
+        messageHistoryLimit: 10,		// set limit to load recent chat history
+        autoEnableSpeechAndTTS: false, 	// set true, to use talkType voice keyboard.
+        graphLib: "d3" ,				// set google, to render google charts.This feature requires loader.js file which is available in google charts documentation.
+        googleMapsAPIKey:"",
+        minimizeMode: true,             // set true, to show chatwindow in minimized mode, If false is set remove #chatContainer style in chatwindow.css  
+        multiPageApp: {
+            enable: false,              //set true for non SPA(Single page applications)
+            userIdentityStore: 'localStorage',//'localStorage || sessionStorage'
+            chatWindowStateStore: 'localStorage'//'localStorage || sessionStorage'
+        },              
+        supportDelayedMessages:true,    // enable to add support for renderDelay in message nodes which will help to render messages with delay from UI       
+        maxTypingIndicatorTime:10000,   //time in milliseconds,typing indicator will be stopped after this time limit,even bot doesn't respond 
+        pickersConfig:{
+            showDatePickerIcon:false,           //set true to show datePicker icon
+            showDateRangePickerIcon:false,      //set true to show dateRangePicker icon
+            showClockPickerIcon:false,          //set true to show clockPicker icon
+            showTaskMenuPickerIcon:false,       //set true to show TaskMenu Template icon
+            showradioOptionMenuPickerIcon:false //set true to show Radio Option Template icon
+        }
+    };
+     /* 
+        allowGoogleSpeech will use Google cloud service api.
+        Google speech key is required for all browsers except chrome.
+        On Windows 10, Microsoft Edge will support speech recognization.
+     */
+
+    KoreSDK.chatConfig=chatConfig
+})(window.KoreSDK);
